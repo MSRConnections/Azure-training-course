b39b785d55c47d2b2b7b3c6278bc68274bc316fc
haojunsheng <haojuns@hotmail.com>
2013-11-17 21:09:30 +0800

    update the lab of Django website

--- Day 1/2. HOL WindowsAzure Web Sites/Source/Exercise02/Begin/DjangoApplication/settings.py
+++ Day 1/2. HOL WindowsAzure Web Sites/Source/Exercise02/Begin/DjangoApplication/settings.py
@@ -1,4 +1,4 @@
-import os
+# import os #uncomment it for the lab
 
 # Django settings for DjangoApplication project.
 
@@ -111,7 +111,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
+
+    # os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'), # uncomment the line to support template
 )
 
 INSTALLED_APPS = (
--- Day 1/2. HOL WindowsAzure Web Sites/Source/Exercise02/Begin/DjangoApplication/urls.py
+++ Day 1/2. HOL WindowsAzure Web Sites/Source/Exercise02/Begin/DjangoApplication/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from DjangoApplication.view import earthquake
+# from DjangoApplication.view import earthquake #uncomment the line to support earthquake view
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -9,7 +9,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'DjangoApplication.views.home', name='home'),
     # url(r'^DjangoApplication/', include('DjangoApplication.DjangoApplication.urls')),
-    ('^earthquake/$',earthquake),
+
+    # ('^earthquake/$',earthquake), #uncomment the line to support earthquake view
+
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
