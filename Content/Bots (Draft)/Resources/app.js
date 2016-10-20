a6be5db175f6d5b6014a84d3d9742c3361f52bee
jeffprosise <jeffpro@wintellect.com>
2016-10-20 16:53:19 +0200

    First draft of Bots module

---
+++ Content/Bots (Draft)/Resources/app.js
@@ -0,0 +1,86 @@
+﻿
+
+var restify = require('restify');
+var builder = require('botbuilder');
+var https = require('https');
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+bot.dialog('/', [
+
+    function (session) {
+        builder.Prompts.text(session, "Hello, and welcome to Factbot! What's your name?");
+    },
+
+    function (session, results) {
+
+        session.userData.name = results.response;
+        builder.Prompts.number(session, "Hi " + results.response + ", before we get started, let me find out a few things about you. How many years have you been coding?");
+    },
+
+    function (session, results) {
+
+        session.userData.yearsCoding = results.response;
+        builder.Prompts.choice(session, "What language do you love the most?", ["C#", "JavaScript", "TypeScript", "Visual FoxPro"]);
+    },
+
+    function (session, results) {
+
+        session.userData.language = results.response.entity;
+        builder.Prompts.choice(session, "What's your favorite midnight snack?", ["Pizza", "Poptarts", "Chicken and waffles", "Kale salad"]);
+    },
+
+    function (session, results) {
+
+        session.userData.snack = results.response.entity;
+        builder.Prompts.confirm(session, "Now that I better understand your personality, would you me to grab a random, interesting fact for you to enjoy?");
+    },
+
+    function (session, results) {
+
+        session.userData.action = results.response;
+
+        if (session.userData.action == true) {
+
+            var optionsget = {
+                host: 'traininglabservices.azurewebsites.net',
+                port: 443,
+                path: '/api/Facts/1',
+                method: 'GET'
+            };
+
+            var reqGet = https.request(optionsget, function (res) {
+                res.on('data', function (factResult) {
+                    session.send(factResult.toString());
+                });
+            });
+
+            reqGet.end();
+            reqGet.on('error', function (e) {
+                console.error(e);
+            });
+
+            session.send(session.userData.name.toUpperCase() + ", DID YOU KNOW:");
+        }
+        else {
+            session.send("Okay, I think I've got it " + session.userData.name +
+                     ": You've been writing code for " + session.userData.yearsCoding + " years," +
+                     " love to use " + session.userData.language + ", and" +
+                     " you prefer " + session.userData.snack + " as a midnight snack.");
+
+        }
+
+    }
+
+]);
